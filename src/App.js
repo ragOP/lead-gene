b789c17653e2b1369c469ce1237e49eebb808d53
farish-jamal <farishjamal98@gmail.com>
2024-05-29 17:58:20 +0530

    Fixes in path

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-// src/App.js
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
+import NotFound from "./components/404";
 
 const App = () => {
-  return <Home />;
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/engmed1" element={<Home />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/components/404.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function NotFound() {
+  return <div style={{ textAlign: "center" }}>404 not found</div>;
+}
