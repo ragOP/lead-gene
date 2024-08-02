ec492d958a9e3051c6ae10e3631d7d22640ab63d
farish-jamal <farishjamal98@gmail.com>
2024-08-02 14:55:34 +0530

    update: Push

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import NotFound from "./components/404";
+import Toast from "./components/Toast.jsx"
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <div className="App">
         <Routes>
           <Route path="/engmed1" element={<Home />} />
+          <Route path="/toast" element={<Toast />} />
           <Route path="/*" element={<NotFound />} />
         </Routes>
       </div>
